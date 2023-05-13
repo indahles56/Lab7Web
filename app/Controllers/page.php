26aead61a64b07fa374a8324706de2d93f09c34c
Indah Lestari <indahlestari05@mhs.pelitabangsa.ac.id>
2023-05-13 16:16:31 +0700

    first commit

---
+++ app/Controllers/page.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        return view('about', [
+            'title' => 'Halaman About',
+            'content' => 'Ini adalah halaman abaut yang menjelaskan tentang isi
+            halaman ini. <br> <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ]);
+    }
+    public function contact()
+    {
+        echo "Ini halaman Contact";
+    }
+    public function faqs()
+    {
+        echo "Ini halaman FAQ";
+    }
+    public function tos()
+    {
+        echo "Ini halaman Term of Services";
+    }
+}
---
+++ app/Views/template/footer.php
@@ -0,0 +1,38 @@
+</section>
+            <div class="widget">
+            <table>
+                <tr>
+                    <th>Widget Header</th>
+                </tr>
+                <tr>
+                    <td><a href="#">Widget Link</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">Widget Link</a></td>
+                </tr>
+            </table>
+
+
+            <div class="widget-text">
+                <table>
+                    <tr>
+                        <th>Widget Text</th>
+                    </tr>
+                    <tr>
+                        <td>
+                    <p>Vestibulum lorem elit, iaculis in nisl volutpat,
+                        malesuada tincidunt arcu. Proin in leo fringilla, vestibulum mi porta,
+                        faucibus felis. Integer pharetra est nunc, nec pretium nunc pretium ac.</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+        </section>
+        <footer>
+            <p>&copy; 2021 - Universitas Pelita Bangsa</p>
+        </footer>
+    </div>
+</body>
+
+</html>
